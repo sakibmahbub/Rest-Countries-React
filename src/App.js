89fd7e58b415e9cb8762c01eb1baed5aed823279
sakibmahbub <arafatsakib48@gmail.com>
2023-03-24 00:30:56 +0600

    displayed country name in UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import Countries from "./components/Countries/Countries";
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
-    </div>
-  );
-}
-
-function LoadCountries() {
-  // Step 1: Set state
-  const [countries, setCountries] = useState([]);
-  // Step 2: Write effect
-  useEffect(() => {
-    // Step 3: Writing fetch here & setting loaded data
-    fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-  return (
-    <div>
-      <h1>Lets Travel Around The World!</h1>
+      <Countries></Countries>
     </div>
   );
 }
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+
+const Countries = () => {
+  // Step 1: Set state
+  const [countries, setCountries] = useState([]);
+  // Step 2: Write effect
+  useEffect(() => {
+    // Step 3: Writing fetch here & setting loaded data
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <div>
+      <h1>Lets Travel Around The World!</h1>
+      <h3>Available Countries : {countries.length}</h3>
+      {countries.map((country) => (
+        <Country name={country.name.common}></Country>
+      ))}
+    </div>
+  );
+};
+
+export default Countries;
