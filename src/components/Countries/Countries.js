234b7beb9ff4ac2a4d149f1b0a9330f762d5a4dc
sakibmahbub <arafatsakib48@gmail.com>
2023-03-24 16:10:54 +0600

    add flag & unique key for mapping components

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
   // Step 1: Set state
@@ -15,9 +16,11 @@ const Countries = () => {
     <div>
       <h1>Lets Travel Around The World!</h1>
       <h3>Available Countries : {countries.length}</h3>
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country country={country} key={country.cca3}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
-  const { name, area, population, region } = props.country;
+  const { name, area, population, region, flags } = props.country;
   return (
     <div className="country">
-      <h2>Country Name: {name.common}</h2>
+      <img src={flags.png} alt="" />
+      <h2>Name: {name.common}</h2>
       <h4>Area : {area}</h4>
       <h4>Population: {population}</h4>
-      <h5>Region: {region}</h5>
-    </div> 
+      <h4>Region: {region}</h4>
+    </div>
   );
 };
 
